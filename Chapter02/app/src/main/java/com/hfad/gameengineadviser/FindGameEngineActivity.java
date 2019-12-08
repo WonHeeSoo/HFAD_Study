97ded24befcae8826e405988f20dc694fb476c27
WHS <wonhs717@gmail.com>
2019-12-08 16:58:30 +0900

    feat: Add Chapter02 project

---
+++ Chapter02/app/src/main/java/com/hfad/gameengineadviser/FindGameEngineActivity.java
@@ -0,0 +1,14 @@
+package com.hfad.gameengineadviser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindGameEngineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_GameEngine);
+    }
+}
