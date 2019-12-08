2a0afe588be9b951b48b171f938682d1316c3c08
WHS <wonhs717@gmail.com>
2019-12-08 18:47:47 +0900

    docs: Update Chapter02 project

--- Chapter02/app/src/main/java/com/hfad/gameengineadviser/FindGameEngineActivity.java
+++ Chapter02/app/src/main/java/com/hfad/gameengineadviser/FindGameEngineActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+
 public class FindGameEngineActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_find_GameEngine);
+        setContentView(R.layout.activity_find_game_engine);
+    }
+
+    public void onClickFindGameEngine(View view)
+    {
+
     }
 }
